fb648427062a184ef169fb50ee6333dde43457c6
Pavel Shnayder <pavel_shnayder@3divi.com>
2013-02-04 22:09:32 +0600

    initial irboard

---
+++ apps/irboard/src/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const string caption =
+		"IR White Board program"
+		;
+const string usage =
+		"Usage: "
+		;
+
+int main( int argc, char* argv[] ){
+
+	cout << caption << endl;
+
+	return 0;
+}
