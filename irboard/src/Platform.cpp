47c920bb5ffaf31fe430d75d4deea23a0b0a2f66
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-09-09 17:34:16 +0400

    added platform class

---
+++ irboard/src/Platform.cpp
@@ -0,0 +1,20 @@
+/*
+ * Platform.cpp
+ *
+ *  Created on: Sep 9, 2014
+ *      Author: pavel
+ */
+
+#include <Platform.h>
+
+Platform::Platform()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Platform::~Platform()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ irboard/src/Platform.h
@@ -0,0 +1,19 @@
+/*
+ * Platform.h
+ *
+ *  Created on: Sep 9, 2014
+ *      Author: pavel
+ */
+
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+class Platform
+{
+public:
+    Platform();
+    virtual ~Platform();
+
+};
+
+#endif /* PLATFORM_H_ */
