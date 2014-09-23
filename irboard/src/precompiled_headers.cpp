0cc6897f5e8fd389dca62ea8e46569a5b9b9c42d
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-09-23 11:00:29 +0400

    trying to add precompiled header to project, so far without success :)

---
+++ irboard/src/precompiled_headers.cpp
@@ -0,0 +1,8 @@
+/*
+ * precompiled_headers.cpp
+ *
+ *  Created on: Sep 23, 2014
+ *      Author: pavel
+ */
+
+#include <precompiled_headers.h>
---
+++ irboard/src/precompiled_headers.h
@@ -0,0 +1,28 @@
+/*
+ * precompiled_headers.h
+ *
+ *  Created on: Sep 23, 2014
+ *      Author: pavel
+ */
+
+#ifndef PRECOMPILED_HEADERS_H_
+#define PRECOMPILED_HEADERS_H_
+
+#include <iostream>
+
+#include <memory>
+#include <functional>
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+
+#include <QtCore/QEvent>
+#include <QtCore/QTimer>
+
+#include <QtGui/QKeyEvent>
+
+#include <QtOpenGL/QGLColormap>
+#include <QtOpenGL/QGLWidget>
+#include <QtOpenGL/QGLFunctions>
+
+#endif /* PRECOMPILED_HEADERS_H_ */
