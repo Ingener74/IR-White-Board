8f1ab980a389b3664e2c645e1d2a5729c0e1f486
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-10-10 09:06:00 +0400

    added ir tester program for creating test video

---
+++ ir-tester/src/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 10, 2014
+ *      Author: pavel
+ */
+
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    try
+    {
+        /*
+         * 1. show monitor image
+         * 2. listen mouse movement
+         * 3. create faik test ir video with calibration and work
+         */
+
+        VideoWriter vw;
+
+        return 0;
+    }
+    catch (exception const & e)
+    {
+        cerr << e.what() << endl;
+        return 1;
+    }
+}
+
